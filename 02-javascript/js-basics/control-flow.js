7db6ad5654489852d3329d2a1d0434385a751fba
Ravi Gadela <ravi.gadela@gmail.com>
2025-12-23 18:00:56 +1100

    JS-Basics

---
+++ 02-javascript/js-basics/control-flow.js
@@ -0,0 +1,93 @@
+//If Statement
+
+let hour = 10;
+
+if(hour >= 6 && hour < 12) {
+    console.log('Good Morning');
+}
+else if (hour >= 12 && hour < 18) {
+    console.log('Good Afternoon');
+}
+else {
+    console.log('Good Evening');
+}
+
+// Switch
+let role = 'guest';
+
+switch(role) {
+    case 'guest': 
+        console.log('guest user');
+        break;
+    case 'moderator':
+        console.log('Moderator');
+        break;
+    case 'admin': 
+        console.log('Admin');
+        break;
+    default: 
+        console.log('Unknown Role');
+}
+
+//For - display only for odd numbers.
+for(let i = 0; i < 5; i++ ) {
+    if (i % 2 !== 0) console.log('Hello World' + i);
+}
+
+//While
+// let i = 0;
+// while(i < 5) {
+//  if (i % 2 !== 0) console.log('Hello World' + i);
+//  i++;
+// }
+
+//Do While - executed at least once - rare. 
+let i = 0;
+do {
+ if (i % 2 !== 0) console.log('Hello World' + i);
+ i++;
+} while(i < 5) 
+
+//For - in Loop - used to iterate over objects. 
+const person2 = {
+    name: 'Ravi',
+    age: 30
+}
+
+for(let key in person2) {
+    console.log(key , person2[key]);
+}
+
+const colors = ['red', 'green', 'blue']
+for(let index in colors) {
+    console.log(index , colors[index]);
+}
+
+//For - of loop - used to iterate over arrays
+for(let color of colors) {
+    console.log(color);
+}
+
+//Break - print only odd numbers 
+// Break breaks the loop and continue jumps to next iteration
+let one = 0;
+while (one <= 10) {
+    if(one % 2 === 0) {
+        one++;
+        continue;
+    }
+    console.log(one);
+    one++ 
+}
+
+//Falsy Values
+// undefined
+// null
+// ''
+// false
+// 0
+// NaN
+
+
+
+
--- 03-vscode/readme.txt
+++ 03-vscode/readme.txt
@@ -9,6 +9,8 @@ h1 + Tab --> Generates the h1 tag
 Ctrl + Tab --> Open the Terminal
 clg + Tab --> console log statement
 
+select the expression and type ( then vscode will put the expression within parenthersis if ( x % 3 == 0 && x%5 === 0 ) 
+move the selection up - select the line - press the Alt key and up arrow
 
 
 
